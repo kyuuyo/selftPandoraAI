59fadcfc2f6e1c697a2c9220c35461d565d93e26
yxg <morty_yang@icloud.com>
2023-03-31 10:15:07 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    server: {
-        port: 3001, // 默认: 3000
-    },
     ssr: false,
     runtimeConfig: {
         public: {
