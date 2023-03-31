38174c0ed14a7b17651715f459468174d40eab62
yxg <morty_yang@icloud.com>
2023-03-31 12:26:39 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
+        baseURL: '/pandora/',
         websiteURL: '/pandora/',
         buildAssetsDir: '/pandora/_nuxt/',
     },
